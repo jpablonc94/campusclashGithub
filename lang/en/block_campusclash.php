fb4af41f0d89f2680009e088632a93dae1943c22
Juan_Pablo_Navarro <jpablonc94@gmail.com>
2015-08-11 13:41:13 +0200

    Primer commit, contiene primeros archivos.php (CampusClash)

---
+++ lang/en/block_campusclash.php
@@ -0,0 +1,5 @@
+<?php
+$string['pluginname'] = 'CampusClash block';
+$string['campusclash'] = 'CampusClash';
+$string['campusclash:addinstance'] = 'Add a new CampusClash block';
+$string['campusclash:myaddinstance'] = 'Add a new CampusClash block to the My Moodle page';
