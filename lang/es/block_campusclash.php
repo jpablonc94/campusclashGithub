8665125507e1bbe8ce89b14c3c0bf36d2ed5951b
Juan_Pablo_Navarro <jpablonc94@gmail.com>
2015-08-11 17:29:38 +0200

    Añado block_campusclash.php a la carpeta lang/es/

---
+++ lang/es/block_campusclash.php
@@ -0,0 +1,41 @@
+<?php
+// This file is part of CampusClash block for Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * CampusClash block configuration form definition
+ *
+ * @package    campusclash
+ * @subpackage block_campusclash
+ * @authors    Juan Pablo Navarro Castillo
+ */
+
+/**
+
+IDIOMA: ESPAÑOL
+
+Este apartado es el llamado language file y es donde se realizan las traducciones de los strings que usamos en otros archivos. Es por decirlo así, una correspondencia. 
+
+*/
+
+$string['pluginname'] = 'CampusClash block';
+$string['campusclash'] = 'CampusClash';
+$string['campusclash:addinstance'] = 'Add a new CampusClash block';
+$string['campusclash:myaddinstance'] = 'Add a new CampusClash block to the My Moodle page';
+
+//Edit form setting
+$string['blockstring'] = 'Bloque String';
+$string['blocktitle'] = 'Bloque Title';
+$string['configuration'] = 'Bloque CampusClash configuration';
